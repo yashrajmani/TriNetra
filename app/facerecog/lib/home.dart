f3bfa92aef9e3925792c17085c7825e6daec912f
Yash Raj Mani <yashrajmani15946@gmail.com>
2022-05-19 15:55:16 +0530

    5: Added image picker
    this version contains the upload screen that allows to select image from gallery/camera.

--- app/facerecog/lib/home.dart
+++ app/facerecog/lib/home.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 import 'package:facerecog/uploadscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,6 +12,26 @@ class Home extends StatefulWidget {
 }
 
 class _Home extends State<Home> {
+  File? image;
+
+  Future pickImage(ImageSource imageSource) async {
+    try {
+      final image = await ImagePicker().pickImage(source: imageSource);
+      if (image == null) {
+        return;
+      } else {
+        print("IMAGE FOUND");
+      }
+
+      final imageTemporary = File(image.path);
+      setState(() {
+        this.image = imageTemporary;
+      });
+    } on PlatformException catch (e) {
+      print("FAILED : $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +49,35 @@ class _Home extends State<Home> {
               )),
         ],
       ),
+      bottomNavigationBar: Container(
+        height: 60,
+        color: Colors.black12,
+        child: InkWell(
+          onTap: () {
+            print("CONFIRM TAPPED");
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => Upload()));
+          },
+          child: Padding(
+            padding: EdgeInsets.only(top: 2.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.check),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Confirm',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
 
       body: Center(
         child: Column(
@@ -40,7 +92,20 @@ class _Home extends State<Home> {
               ),
             ),
             SizedBox(
-              height: 50,
+              height: 10,
+            ),
+            image != null
+                ? Image.file(
+                    image!,
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  )
+                : FlutterLogo(
+                    size: 150,
+                  ),
+            SizedBox(
+              height: 10,
             ),
             Text(
               "Use Camera",
@@ -49,14 +114,13 @@ class _Home extends State<Home> {
               ),
             ),
             SizedBox(
-              height: 20,
+              height: 5,
             ),
             ElevatedButton(
               //TODO: ADD CAMERA OPENER!
               onPressed: () {
+                pickImage(ImageSource.camera);
                 print("Camera Pressed!");
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Upload()));
               },
 
               child: Icon(
@@ -65,16 +129,13 @@ class _Home extends State<Home> {
               ),
             ),
             SizedBox(
-              height: 25,
+              height: 5,
             ),
             Divider(
               thickness: 4,
               height: 20,
               indent: 20,
               endIndent: 20,
-            ),
-            SizedBox(
-              height: 25,
             ),
             Text(
               "Use Gallery",
@@ -83,14 +144,13 @@ class _Home extends State<Home> {
               ),
             ),
             SizedBox(
-              height: 20,
+              height: 5,
             ),
             ElevatedButton(
               onPressed: () {
                 //TODO: ADD GALLERY OPENER
                 print("Gallery Pressed!");
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => Upload()));
+                pickImage(ImageSource.gallery);
               },
               child: Icon(
                 Icons.photo_sharp,
